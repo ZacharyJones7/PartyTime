174418aba6792cff3caa73226743bca8da92bfb8
ZacharyJones7 <60017021+ZacharyJones7@users.noreply.github.com>
2020-02-27 11:49:54 -0800

    Now it's working. TODO: add pictures

--- PartyTime/Controllers/HomeController.cs
+++ PartyTime/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PartyInvites.Models;
+
 namespace PartyInvites.Controllers
+
 {
     public class HomeController : Controller
     {
@@ -9,11 +13,30 @@ namespace PartyInvites.Controllers
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
-            }
-        public ViewResult RSVP()
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View();
         }
+        [HttpPost]
+        public ViewResult RsvpForm(Models.GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Models.Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
     }
-    
+
 }
---
+++ PartyTime/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Who even are you?")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "How else am I supposed to send this super sick eVite?")]
+        [RegularExpression(".+\\@.+\\..+",
+ErrorMessage = "Stop messing around.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "It doesn't even have to be a real number but idk how to remove this field, so")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "I need to know who my REAL friends are, so be a real pal and say if you're coming or not")]
+        public bool? WillAttend { get; set; }
+    }
+}
